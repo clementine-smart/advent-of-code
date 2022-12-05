4690c0a6a0a3b05263ede7357ef2631be541d6c2
clementine-smart <cmwsmart@gmail.com>
2022-12-05 23:09:50 +1300

    Solve day 2 part 2

--- src/days/2/day2.ts
+++ src/days/2/day2.ts
@@ -28,7 +28,7 @@ function calculateWinner(numberedGames: number[]): number {
 }
 
 function calculateMyScore(allGames: number[][]): number {
-  let score = 0
+  let score: number = 0
   allGames.map((game) => {
     if (calculateWinner(game) === 6)
       return score = score + game[1] + 6
@@ -41,4 +41,43 @@ function calculateMyScore(allGames: number[][]): number {
   return score
 }
 
-console.log(calculateMyScore(formatInput(input)))
+function formatInputStrategy2(input: string): number[][] {
+  const games: string[][] = input.split(/\r?\n/).map((game) => {
+    return game.split(' ')
+  })
+  return games.map((game) => {
+   return game.map((hand) => {
+    if (hand === 'A')
+      return 1
+    if (hand === 'B')
+      return 2
+    if (hand === 'C')
+      return 3
+    if (hand === 'X')
+      return 0
+    if (hand === 'Y')
+      return 3
+    return 6
+   })
+  })
+}
+
+function calculatePlay(games: number[][]): number[][]{
+  return games.map((game) => {
+      if (game[1] == 0 && game[0] !== 1)
+        return [game[0], game[0] - 1]
+      if (game[1] == 0 && game[0] === 1)
+        return [game[0], 3]
+      if (game[1] == 3)
+        return [game[0], game[0]]
+      if (game[1] == 6 && game[0] !== 3)
+        return [game[0], game[0] + 1]
+      return [game[0], 1]
+    })
+}
+
+const strategy2: number[][] = formatInputStrategy2(input)
+const strategy1Score: number = calculateMyScore(formatInput(input))
+const strategy2Score: number = calculateMyScore(calculatePlay(strategy2))
+console.log(strategy1Score)
+console.log(strategy2Score)
