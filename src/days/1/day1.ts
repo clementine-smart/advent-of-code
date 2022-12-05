d9374673db523e5625ad25b0db4b24e1584e0d76
clementine-smart <cmwsmart@gmail.com>
2022-12-05 16:45:18 +1300

    Solve day 1 part 2

--- src/days/1/day1.ts
+++ src/days/1/day1.ts
@@ -2,16 +2,20 @@ import { getInput } from "../../utils"
 
 const input = getInput(__dirname, '1')
 
-function calculateCalories(input: string): number {
+function splitCalories(input: string): number[] {
   const caloriesInput: string[] =  input.split(/\r?\n/)
   const calories = caloriesInput.map((calorie) => {
     return Number(calorie)
   })
+  return calories
+}
+
+function calculateHighestCalories(input: number[]): number {
   let temp: number = 0
   let highest: number = 0
-  for (let i = 0; i < calories.length; i++){
-    if(calories[i] !== 0) {
-      temp = temp + calories[i]
+  for (let i = 0; i < input.length; i++){
+    if(input[i] !== 0) {
+      temp = temp + input[i]
     } else {
       if(temp > highest) {
         highest = temp
@@ -23,5 +27,24 @@ function calculateCalories(input: string): number {
   return highest
 }
 
-console.log(calculateCalories(input))
+function calculate3HighestCalories(input: number[]): number {
+  let temp: number = 0
+  let highest: number[] = [0, 0, 0]
+  for (let i = 0; i < input.length; i++){
+    if(input[i] !== 0) {
+      temp = temp + input[i]
+    } else {
+      if(temp > Math.min(...highest)) {
+        const lowest = highest.findIndex((calorie) => calorie == Math.min(...highest))
+        highest[lowest] = temp
+        temp = 0
+      }
+      temp = 0
+    }
+  }
+  return highest.reduce((a, b) => a + b)
+}
+
+console.log(calculateHighestCalories(splitCalories(input)))
+console.log(calculate3HighestCalories(splitCalories(input)))
 
