0d394e293efb09b6b57f78151020e87f28547329
clementine-smart <cmwsmart@gmail.com>
2022-12-05 15:22:49 +1300

    Solve day 1 part 1

--- src/days/1/day1.ts
+++ src/days/1/day1.ts
@@ -1,3 +1,27 @@
 import { getInput } from "../../utils"
 
-console.log(getInput(__dirname, '1'))
+const input = getInput(__dirname, '1')
+
+function calculateCalories(input: string): number {
+  const caloriesInput: string[] =  input.split(/\r?\n/)
+  const calories = caloriesInput.map((calorie) => {
+    return Number(calorie)
+  })
+  let temp: number = 0
+  let highest: number = 0
+  for (let i = 0; i < calories.length; i++){
+    if(calories[i] !== 0) {
+      temp = temp + calories[i]
+    } else {
+      if(temp > highest) {
+        highest = temp
+        temp = 0
+      }
+      temp = 0
+    }
+  }
+  return highest
+}
+
+console.log(calculateCalories(input))
+
