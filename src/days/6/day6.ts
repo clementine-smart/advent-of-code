453b8d599675cdbeb72d89b22d0dfabdfee675a0
clementine-smart <cmwsmart@gmail.com>
2022-12-06 22:56:39 +1300

    Solve day 6 part 2

--- src/days/6/day6.ts
+++ src/days/6/day6.ts
@@ -2,18 +2,27 @@ import { getInput } from '../../utils'
 
 const input: string = getInput(__dirname, '6')
 
-function findMarker(input: string) {
-  const markStart = input.split('').map((letter, i, arr) => {
-    if (letter !== arr[i + 1] && letter !== arr[i + 2] && arr[i + 1] !== arr[i + 2] && letter !== arr[i + 3] && arr[i + 1] !== arr[i + 3] && arr[i + 2] !== arr[i + 3]) {
-      return i + 4
-    }
-   return letter
-  })
-  return markStart.find((letter) => {
-    return typeof letter == 'number'
-  })
+function findMarker(input: string, startOfPack: number) {
+  const signals: string[] = input.split('')
+  let testSignals: string[] = []
+  let count: number = 0
+  let marker: number = 0
+  for (let i = 0; i < signals.length; i++) {
+    testSignals = signals.slice(i, i + startOfPack)
+    for (let j = 0; j < startOfPack; j++) {
+      if (testSignals.filter((signal) => signal == signals[i + j]).length <= 1) {
+        count++
+        if (count === startOfPack) {
+          marker = i + startOfPack
+          return marker
+        }
+      }
+    } 
+    count = 0
+  }
+  return marker
 }
 
-const marker = findMarker(input)
+const marker = findMarker(input, 14)
 
 console.log(marker)
